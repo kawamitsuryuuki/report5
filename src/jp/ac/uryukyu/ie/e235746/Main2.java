26abbade7d14998cbde4f905583fd79b9223a59d
Kawamitu ryuuki <e235746@ie.u-ryukyu.ac.jp>
2023-12-11 14:38:58 +0900

    step3

---
+++ src/jp/ac/uryukyu/ie/e235746/Main2.java
@@ -0,0 +1,9 @@
+package jp.ac.uryukyu.ie.e235746;
+
+public class Main2.{
+    public static void main(String[] args){
+    String str = "壱百満"
+    int value = Integer.parseInt(str);
+    System.out.println(value);
+    }
+}
