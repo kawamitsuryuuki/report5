a192a150a260c45e553fea554fddba90e3f7c3a1
Kawamitu ryuuki <e235746@ie.u-ryukyu.ac.jp>
2023-12-11 14:23:48 +0900

    step1

--- src/jp/ac/uryukyu/ie/e235746/Main.java
+++ src/jp/ac/uryukyu/ie/e235746/Main.java
@@ -1,12 +1,9 @@
 package jp.ac.uryukyu.ie.e235746;
 
 public class Main{
-    try{
+    public static void main(String[] args){
     String str = null;
     System.out.println(str.length());
-    }catch(NullPointerException e){
-        System.out.println("NullPointerExceptionが発生しました");
-        System.out.println(e.getMessage());
     }
 }
 
